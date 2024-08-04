6d55806f5a39561a1483c752e2daa9b5a85e6490
amiramahmedjan <mahmedjan.amira@gmail.com>
2024-08-04 13:39:23 -0700

    Initial Commit

---
+++ Health Data Visualizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Health Data Visualizer
+//
+//  Created by Amira Mahmedjan on 04.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Health Data Visualizer/Health_Data_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_Data_VisualizerApp.swift
+//  Health Data Visualizer
+//
+//  Created by Amira Mahmedjan on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Health_Data_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
